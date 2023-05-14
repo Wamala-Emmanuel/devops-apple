1998c3415c78c80ec002b2833da38ad6e91ac436
Wamala Emmanuel Nsubuga <kisaakye.emma@gmail.com>
2023-05-14 12:03:20 +0300

    remove sensitive values

--- terraform.auto.tfvars
+++ terraform.auto.tfvars
@@ -7,10 +7,3 @@ tags = {
 
 resource_group_name = "wamala-devops"
 
-subscription_id = "edb48b4f-7407-4639-b37f-b60931c2ee1d"
-
-# tenant_id = ""
-
-client_id = "7b324b24-279c-4863-b8d4-260063357b1f"
-
-client_secret = ".lj8Q~IYAvh6p2gHJY6_uNU1IE6nZv0s1EySNbxF"
--- variable.tf
+++ variable.tf
@@ -12,16 +12,20 @@ variable "resource_group_name" {
 
 variable "subscription_id" {
   type = string
+  sensitive = true
 }
 
-# variable "tenant_id" {
-#   type = string
-# }
+variable "tenant_id" {
+  type = string
+  sensitive = true
+}
 
 variable "client_id" {
   type = string
+  sensitive = true
 }
 
 variable "client_secret" {
   type = string
+  sensitive = true
 }
